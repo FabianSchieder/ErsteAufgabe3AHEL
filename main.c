d14e2a16f21c3b673ce3c55c413169a803f49e2e
Fabian Schieder <fabian.schieder@el.htlhl.at>
2024-09-05 22:02:24 +0200

    V01

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 struct Student
@@ -9,11 +10,16 @@ struct Student
 };
 
 int printMenu();
-void addStudents();
+struct Student* addStudents(struct Student* students, int* size);
+void printStudents(struct Student* students, size_t size);
+struct Student* getStudentsFile(int* size);
+void writeStudentsFile(struct Student* students, size_t size);
 
 int main(void)
 {
     int option = 0;
+    int size = 0;
+    struct Student* students = NULL;
 
     do
     {
@@ -22,14 +28,29 @@ int main(void)
         switch(option)
         {
             case 1:
-                addStudents();
+                students = addStudents(students, &size);
                 break;
+
+            case 2:
+                printStudents(students, size);
+                break;
+
+            case 3:
+                students = getStudentsFile(&size);
+                break;
+
+            case 4:
+                writeStudentsFile(students, size);
+                break;
+
             default:
                 break;
         }
     } while(option != 0);
 
-    return 0;
+    free(students);
+
+return EXIT_SUCCESS;
 }
 
 int printMenu()
@@ -42,22 +63,94 @@ int printMenu()
     return option;
 }
 
-void addStudents()
+struct Student* addStudents(struct Student* students, int* size)
 {
-    int amount;
+    int newStudentsCount = 0;
+    int newSize = 0;
 
     printf("Wieviele Schueler wollen Sie hinzufuegen?\nEingabe: ");
-    scanf("%i", &amount);
+    scanf("%i", &newStudentsCount);
 
-    struct Student students[amount];
+    newSize = *size + newStudentsCount;
+    students = realloc(students, newSize * sizeof(struct Student));
 
-    for(int i = 0; i < amount; i++)
+    for(int i = *size; i < newSize; i++)
     {
-        printf("Nummer (Schueler %i): ", i);
-        scanf("%d", students[i].number);
-        printf("Vorname (Schueler %i): ", i);
+        students[i].number = i + 1;
+        printf("Vorname (Schueler %i): ", i + 1);
         scanf("%s", students[i].firstName);
-        printf("Nachname (Schueler %i): ", i);
+        printf("Nachname (Schueler %i): ", i + 1);
         scanf("%s", students[i].lastName);
     }
+
+    *size = newSize;
+    return students;
+}
+
+void printStudents(struct Student* students, size_t size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        printf("Schueler %i: Nummer: %d, Vorname: %s, Nachname: %s\n", i + 1, students[i].number, students[i].firstName, students[i].lastName);
+    }
+}
+
+struct Student* getStudentsFile(int* size)
+{
+    char filename[100];
+    int count = 0;
+
+    printf("Pfad: ");
+    scanf("%s", filename);
+
+    FILE* file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        printf("Fehler beim Oeffnen der Datei.\n");
+        return NULL;
+    }
+
+    struct Student* students = NULL;
+    struct Student temp;
+
+
+    while (fscanf(file, "%d %19s %19s", &temp.number, temp.firstName, temp.lastName) == 3)
+    {
+        count++;
+        students = realloc(students, count * sizeof(struct Student));
+        if (students == NULL)
+        {
+            printf("Fehler beim Zuweisen von Speicher.\n");
+            fclose(file);
+            return NULL;
+        }
+        students[count - 1] = temp;
+    }
+
+    *size = count;
+    fclose(file);
+    return students;
+}
+
+void writeStudentsFile(struct Student* students, size_t size)
+{
+    char filename[100];
+    printf("Pfad: ");
+    scanf("%s", filename);
+
+    FILE* file = fopen(filename, "w");
+
+    if (file == NULL)
+    {
+        printf("Fehler beim Oeffnen der Datei zum Schreiben.\n");
+        return;
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        fprintf(file, "%d %s %s\n", students[i].number, students[i].firstName, students[i].lastName);
+    }
+
+    fclose(file);
+    printf("Schuelerdaten wurden erfolgreich in die Datei geschrieben.\n");
 }
